a02e5cd7cc491f9d019214a11dd598ef3fc8ffa1
Trijal-Chaudhary <harshvardhanchaudhary86@gmail.com>
2025-03-10 15:38:19 +0530

    added js for check winner

--- script/script.js
+++ script/script.js
@@ -6,18 +6,49 @@ const oTurn = document.querySelector('.js-oturn')
 let player2 = true;
 buttons.forEach((button)=>{
   button.addEventListener('click', ()=>{
-    if(player2){
-      button.innerHTML = 'X'
-      button.disabled = true;
-      player2 = false;
-      xTurn.classList.add('opasity0');
-      oTurn.classList.remove('opasity0')
-    }else{
-      button.innerHTML = 'O'
-      button.disabled = true;
-      player2 = true;
-      oTurn.classList.add('opasity0');
-      xTurn.classList.remove('opasity0')
+    if(!checkWinner()){
+      if(player2){
+        button.innerHTML = 'X'
+        button.disabled = true;
+        player2 = false;
+        checkWinner()
+        xTurn.classList.add('opasity0');
+        oTurn.classList.remove('opasity0')
+      }else{
+        button.innerHTML = 'O'
+        button.disabled = true;
+        player2 = true;
+        checkWinner()
+        oTurn.classList.add('opasity0');
+        xTurn.classList.remove('opasity0')
+      }
     }
   })
+  
 })
+//pattern
+const winPatern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]];
+//check winner
+function checkWinner(){
+  for(let i = 0; i < winPatern.length; i++){
+    let patern = winPatern[i]
+    let first = patern[0];
+    let second = patern[1];
+    let third = patern[2];
+    if(buttons[first].innerHTML != '' && buttons[second].innerHTML != '' && buttons[third].innerHTML != ''){
+      if(buttons[first].innerHTML === buttons[second].innerHTML && buttons[second].innerHTML === buttons[third].innerHTML){
+        console.log('mil gaya winner')
+        return true;
+      }
+    }
+  }
+  return false;
+};
